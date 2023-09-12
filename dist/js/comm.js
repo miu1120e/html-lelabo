603933e986df796b48c88291425f2c4ef72d60fb
ltnal34 <chae1120e@gmail.com>
2023-09-12 13:14:28 +0900

    9.12

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -36,3 +36,98 @@ $(document).ready(function () {
     $(".product-detail-main-text").slideToggle("slow");
   });
 });
+// add
+$(function () {
+  let mm = gsap.matchMedia();
+
+  mm.add("(min-width: 1801px)", () => {
+    gsap.to(".sliding-cover", {
+      duration: 2.3,
+      delay: 0.4,
+      x: "-61vw",
+      ease: CustomEase.create(
+        "custom",
+        "M0,0 C0.016,0.406 0.161,0.83 0.412,0.918 0.59,0.98 0.766,1 1,1 "
+      ),
+    });
+  });
+
+  mm.add("(max-width: 1800px)", () => {
+    gsap.to(".sliding-cover", {
+      duration: 2.3,
+      delay: 0.4,
+      x: "-57vw",
+      ease: CustomEase.create(
+        "custom",
+        "M0,0 C0.016,0.406 0.161,0.83 0.412,0.918 0.59,0.98 0.766,1 1,1 "
+      ),
+    });
+  });
+
+  mm.add("(max-width: 1400px)", () => {
+    gsap.to(".sliding-cover", {
+      duration: 2.3,
+      delay: 0.4,
+      x: "-54vw",
+      ease: CustomEase.create(
+        "custom",
+        "M0,0 C0.016,0.406 0.161,0.83 0.412,0.918 0.59,0.98 0.766,1 1,1 "
+      ),
+    });
+  });
+
+  mm.add("(max-width: 1100px)", () => {
+    gsap.to(".sliding-cover", {
+      duration: 2.3,
+      delay: 0.4,
+      x: "-50vw",
+      ease: CustomEase.create(
+        "custom",
+        "M0,0 C0.016,0.406 0.161,0.83 0.412,0.918 0.59,0.98 0.766,1 1,1 "
+      ),
+    });
+  });
+
+  gsap.to(".standard-page-header h1", {
+    duration: 1.5,
+    delay: 0.6,
+    opacity: "100%",
+    x: 0,
+    ease: "ease-in-out",
+  });
+
+  gsap.to(".standard-hero-text", {
+    duration: 1.5,
+    delay: 1,
+    opacity: "100%",
+    x: 0,
+    ease: "ease-in-out",
+  });
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.to(".sliding-cover", {
+    scrollTrigger: {
+      trigger: ".content-area section:nth-child(2)",
+      start: "clamp(top 85%)",
+      end: "top top",
+      scrub: 2,
+      markers: true,
+      invalidateOnRefresh: true,
+    },
+    width: "200vw",
+    ease: "ease-in-out",
+  });
+
+  gsap.to(".standard-hero-content-flex", {
+    scrollTrigger: {
+      trigger: ".content-area section:nth-child(2)",
+      start: "clamp(top 85%)",
+      end: "top top",
+      scrub: 2,
+    },
+    opacity: 0,
+    ease: "ease-in-out",
+    x: "-30vw",
+  });
+});
---
+++ images/main/about.php
@@ -0,0 +1,228 @@
+<?php
+include_once('./_common.php');
+
+// 페이지 제목 (웹접근성 관련)
+$g5['title'] = '단체소개';
+
+add_stylesheet('<link rel="stylesheet" href="'.G5_CSS_URL.'/sub.css">', 0);
+include_once(G5_PATH.'/head.php');
+?>
+
+
+
+<div class="category-info">
+            <div class="heading">
+              <h2 class="best-seller">LELABO LAB</h2> 
+            </div>
+            <div class="description">
+              <p>An air of curiosity … in a bottle. We find the wondrous in every location, every moment 
+                – and that's how Diptyque eaux de parfums are born. Each is like a landscape of fragrances, f
+                or you to discover or give as a gift.</p> 
+            </div>
+        </div>
+        <div class="slide-container">
+          <div class="slide-asset-container">
+            <video autoplay="" playsinline="" muted="" loop="" class="bgvid">
+              <source src="https://player.vimeo.com/progressive_redirect/playback/768653107/rendition/720p/file.mp4?loc=external&amp;signature=bf4e5562a729fed80c50265aacddc2e395cb1d49d2a797d8b809a8ed33d2e834" type="video/mp4">
+            </video>
+          </div>
+        </div>
+        <div class="section-about">
+          <div class="shell">
+            <section class="section-g section-event2">
+              <h2 class="section-tit">LELABO LAB</h2>
+              <div class="event1">
+                  <img src="./images/intro1.png" alt="ambroxyde" />
+                  <div class="contents-txt">
+                  <p>LELABO LAB</p>
+                  <a href="#" class="btn-10">DISCOVER</a>
+                </div>
+              </div>
+            </section>
+            <section class="section-g section-s">
+              <h2 class="section-tit">AT YOUR SERVICE</h2>
+              <div class="section-contents">
+                <ul class="list-service img-zoom">
+                  <li>
+                    <a href="#">
+                    <div class="thumb">
+                        <img src="./images/s1.png" alt="best2">
+                      </div>
+                      <div class="txt">
+                        <strong>프레쉬 블렌딩</strong>
+                        <p>원하는 르 라보의 향을 선택하고, 
+                          수작업으로 신선하게 블렌딩된  향수를 
+                          만날 수 있습니다.</p>
+                        </div>
+                      </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                    <div class="thumb">
+                        <img src="./images/s2.png" alt="best3">
+                      </div>
+                      <div class=" txt">
+                        <strong>퍼스널 라벨링</strong>
+                        <p>18자의 원하는 문구를 크리에이션에 담을 수 있습니다. 
+                          이 경험은 전 세계 르 라보
+                          매장에서 동일하게 제공됩니다.</p>
+                        </div>
+                      </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                    <div class="thumb">
+                        <img src="./images/s3.png" alt="best4">
+                      </div>
+                      <div class="txt">
+                        <strong>각인 서비스</strong>
+                        <p>트래블 튜브 케이스에 이니셜을 새길 수 
+                          있습니다. </p>
+                        </div>
+                      </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                    <div class=" thumb">
+                        <img src="./images/s4.png" alt="best1">
+                      </div>
+                      <div class="txt">
+                        <strong>리필</strong>
+                        <p>원하는 르 라보의 향을 선택하고, 
+                          수작업으로 신선하게 블렌딩된 
+                          향수를 만날 수 있습니다.</p>
+                        </div> 
+                      </a>
+                  </li>
+                </ul>
+              </div>
+                </section>
+            <section class="section-g section-event3">
+              <h2 class="section-tit">LELABO COMUNITY</h2>
+              <div class="event1">
+                  <img src="./images/intro2.png" alt="intro2" />
+                  <div class="contents-txt">
+                  <p>LELABO COMUNITY</p>
+                  <a href="#" class="btn-10">DISCOVER</a>
+                </div>
+              </div>
+            </section>
+            <section class="section-g section-event4">
+              <h2 class="section-tit">LELABO 매장찾기</h2>
+              <div class="event1">
+                  <img src="./images/intro4.png" alt="intro4" />
+                  <div class="contents-txt">
+                  <p>매장찾기</p>
+                  <a href="#" class="btn-10">DISCOVER</a>
+                </div>
+              </div>
+            </section>
+            <section class="section-g section-s">
+              <h2 class="section-tit">COLLECTION</h2>
+              <div class="section-contents">
+                <ul class="list-service img-zoom">
+                  <li>
+                    <a href="#">
+                    <div class="thumb">
+                        <img src="./images/col1.png" alt="best2">
+                      </div>
+                      <div class="txt">
+                        <strong>바디-헤어-페이스</strong>
+                        <p>자세히보기</p>
+                        </div>
+                      </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                    <div class="thumb">
+                        <img src="./images/col2.png" alt="best3">
+                      </div>
+                      <div class=" txt">
+                        <strong>프레그런스</strong>
+                        <p>자세히보기</p>
+                        </div>
+                      </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                    <div class="thumb">
+                        <img src="./images/col3.png" alt="best4">
+                      </div>
+                      <div class="txt">
+                        <strong>홈</strong>
+                        <p>자세히보기</p>
+                        </div>
+                      </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                    <div class=" thumb">
+                        <img src="./images/col4.png" alt="best1">
+                      </div>
+                      <div class="txt">
+                        <strong>시티 익스클루시브</strong>
+                        <p>자세히보기</p>
+                        </div> 
+                      </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="section-contents">
+                <ul class="list-service img-zoom">
+                  <li>
+                    <a href="#">
+                    <div class="thumb">
+                        <img src="./images/col5.png" alt="best2">
+                      </div>
+                      <div class="txt">
+                        <strong>디스커버리 세트</strong>
+                        <p>자세히보기</p>
+                        </div>
+                      </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                    <div class="thumb">
+                        <img src="./images/col6.png" alt="best3">
+                      </div>
+                      <div class="txt">
+                        <strong>디스커버리</strong>
+                        <p>자세히보기</p>
+                        </div>
+                      </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                    <div class="thumb">
+                        <img src="./images/col7.png" alt="best4">
+                      </div>
+                      <div class="txt">
+                        <strong>라이프스타일</strong>
+                        <p>자세히보기</p>
+                        </div>
+                      </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                    <div class="thumb">
+                        <img src="./images/col8.png" alt="best1">
+                      </div>
+                      <div class="txt">
+                        <strong>그루밍</strong>
+                        <p>자세히보기</p>
+                        </div> 
+                      </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="discovery-e">
+                <a href="#" class="btn-13">
+                  당신의 향을 찾아보세요.
+                </a>
+              </div>
+                </section>
+          </div>
+        </div>
+
+<?php
+include_once(G5_PATH.'/tail.php');
